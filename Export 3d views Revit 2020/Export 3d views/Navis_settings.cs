d15a52cc2692cf21aa45501c6c705199bdc1777a
dbecher84 <dbecher84@gmail.com>
2020-11-17 15:28:20 -0500

    Created 2020 toolbar

---
+++ Export 3d views Revit 2020/Export 3d views/Navis_settings.cs
@@ -0,0 +1,121 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Export_3d_views
+{
+    public partial class Select_Settings : Form
+    {
+        
+        public Select_Settings()
+        {
+            InitializeComponent();
+        }
+
+        public string rdoChecked { get; set; }
+        public string LrdoChecked { get; set; }
+
+        public string MChecked(string coordinate)
+        {
+            if (coordinate == "internal")
+            {
+                rdoChecked = "internal";
+                //MessageBox.Show(rdoChecked);
+                return rdoChecked;
+            }
+            else
+            {
+                rdoChecked = "shared";
+                //MessageBox.Show(rdoChecked);
+                return rdoChecked;
+            }
+        }
+
+        public string LChecked(string links)
+        {
+            if (links == "yes")
+            {
+                LrdoChecked = "yes";
+                //MessageBox.Show(rdoChecked);
+                return LrdoChecked;
+            }
+            else
+            {
+                LrdoChecked = "no";
+                //MessageBox.Show(rdoChecked);
+                return LrdoChecked;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                MChecked("internal");
+            }
+            else if (radioButton2.Checked)
+            {
+                MChecked("shared");
+            }
+
+            if (radioButton3.Checked)
+            {
+                LChecked("yes");
+            }
+            else if (radioButton4.Checked)
+            {
+                LChecked("no");
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                //MessageBox.Show("Shared coordnates selected.");
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                //MessageBox.Show("Internal coordnates selected.");
+            }
+        }
+
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked == true)
+            {
+                //MessageBox.Show("Linked Files Exporting.");
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked == true)
+            {
+                //MessageBox.Show("Linked Files not Exporting.");
+            }
+        }
+
+        private void Select_Coordinates_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Export 3d views Revit 2020/Export 3d views/export3d.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Drawing;
+
+namespace export3dviews
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class export3d : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            //Get application and documnet objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+                        
+            //Navisworks export settings
+            NavisworksExportOptions opt = new NavisworksExportOptions
+            {
+                ExportScope = NavisworksExportScope.View,
+                Coordinates = NavisworksCoordinates.Internal,
+                DivideFileIntoLevels = true,
+                ExportUrls = false,
+                ExportRoomGeometry = false,
+                ConvertElementProperties = true,
+                ExportElementIds = true,
+                ExportLinks = false
+            };
+
+            //Initiates Radio buttons form for choosing export coordinates
+            var f = new Export_3d_views.Select_Settings();
+            f.ShowDialog();
+
+            //sets coordinates setting based on buttons in form
+            if (f.rdoChecked == "shared")
+            {
+                opt.Coordinates = NavisworksCoordinates.Shared;
+                //TaskDialog.Show("Coordinates", "Using" + " " + opt.Coordinates.ToString() + " " + "Coordinates");
+            }
+            else
+            {
+                //TaskDialog.Show("Coordinates", "Using" + " " + opt.Coordinates.ToString() + " " + "Coordinates");
+            }
+
+            //sets Export linked files setting based on buttons in form
+            if (f.LrdoChecked == "yes")
+            {
+                opt.ExportLinks = true;
+                //TaskDialog.Show("Linked Files", "Linked files will be exported");
+            }
+            else
+            {
+                //TaskDialog.Show("Linked Files", "Linked files will NOT be exported");
+            }
+
+            //collect #d views in revit
+            FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
+            viewCollector.OfClass(typeof(View3D));
+
+            //list of views to send to the from
+            List<string> listViewNames = new List<string>();
+            foreach (View3D view in viewCollector)
+            {
+                listViewNames.Add(view.Name);
+            }
+
+            //initate list of views form
+            var l = new Export_3d_views.ViewList(listViewNames);
+            l.ShowDialog();
+
+
+            //select save folder
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+
+            //list of views exported
+            List<string> threeDlist = new List<string>();
+
+            //check if revit view is in list and export to NWC if it is
+            if (l.ExportViewList.Count() > 0)
+            {
+                foreach (View3D view in viewCollector)
+                {
+                    foreach (var item in l.ExportViewList)
+                        if (view.Name == item)
+                        {
+                            opt.ViewId = view.Id;
+                            threeDlist.Add(view.Name);
+                            doc.Export(folderPath, view.Name + ".nwc", opt);
+                        }
+                }
+            }
+            else
+            {
+                TaskDialog.Show("View Error", "Must select one view for export");
+                l.ShowDialog();
+            }
+
+            //Display form listing views that were exported
+            var d = new Export_3d_views.ExportedList(threeDlist);
+            d.ShowDialog();
+
+
+            return Result.Succeeded;
+        }
+    }
+}
