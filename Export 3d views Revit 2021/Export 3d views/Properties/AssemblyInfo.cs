685c2d3485ac2b2052f90a753d7dd36c8bdf24e2
dbecher84 <dbecher84@gmail.com>
2021-11-04 16:25:00 -0400

    File Version Info

--- Export 3d views Revit 2021/Export 3d views/Properties/AssemblyInfo.cs
+++ Export 3d views Revit 2021/Export 3d views/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Export 3d views")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Export 3D views to NWC")]
+[assembly: AssemblyDescription("Export multiple 3D views to NWC and uses view name as file name")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Export 3d views")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2021.0.0.0")]
+[assembly: AssemblyFileVersion("2021.0.0.0")]
